466417545dd8aa52146ee59950131d54fd917ad5
acs027 <alicihansarac@gmail.com>
2025-02-09 14:52:24 +0300

    Initial Commit

---
+++ FlightComputer/FlightComputerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightComputerApp.swift
+//  FlightComputer
+//
+//  Created by ali cihan on 9.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlightComputerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
